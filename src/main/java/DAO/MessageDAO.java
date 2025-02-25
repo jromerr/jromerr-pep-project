3e35a855dc0bc0b06dec946c30af0639cb417969
Jose Romero <romer243@csusm.edu>
2025-02-25 23:35:41 +0000

    Add messageDAO method implementation

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -19,6 +19,23 @@ public class MessageDAO {
     }
     
     public Message insertMessage(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO message (posted_by,message_text,time_posted_epoch) VALUES (?,?,?)";
+            PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            pstmt.setInt(1, message.getPosted_by());
+            pstmt.setString(2, message.getMessage_text());
+            pstmt.setLong(3, message.getTime_posted_epoch());
+            pstmt.executeUpdate();
+            ResultSet pkeyResultSet = pstmt.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getLong(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
