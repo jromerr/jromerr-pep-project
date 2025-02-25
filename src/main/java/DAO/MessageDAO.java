b9983e890207f630bfe27016a82b6273f77cf3c7
Jose Romero <romer243@csusm.edu>
2025-02-25 22:41:25 +0000

    Add messageDAO method signatures

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,37 @@
 package DAO;
 
+import Model.Message;
+
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageDAO {
+
+    public List<Message> getAllMessages(){
+        return null;
+    }
+
+    public List<Message> getAllMessagesByAccountId(){
+        return null;
+    }
     
+    public Message insertMessage(Message message){
+        return null;
+    }
+
+    public Message getMessageByMessageId(int msgId){
+        return null;
+    }
+
+    public Message deleteMessageByMessageId(int msgId){
+        return null;
+    }
+
+    public Message updateMessageByMessageId(int msgId){
+        return null;
+    }
+
 }
