157effce090d4c820964341888f9d1b06c55876b
Jose Romero <romer243@csusm.edu>
2025-02-25 22:56:35 +0000

    Add insertAccount method implementation

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -14,6 +14,22 @@ public class AccountDAO {
     }
 
     public Account insertAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username,password) VALUES (?,?)";
+            PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            pstmt.setString(1, account.getUsername());
+            pstmt.setString(2, account.getPassword());
+            pstmt.executeUpdate();
+            ResultSet pkeyResultSet = pstmt.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
