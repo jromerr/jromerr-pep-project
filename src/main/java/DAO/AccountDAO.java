e3bfe8b288d1bc4051f65fad98fde0755a823e31
Jose Romero <romer243@csusm.edu>
2025-02-25 22:26:02 +0000

    Add accountDAO method signatures

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,5 +1,27 @@
 package DAO;
 
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountDAO {
-    
+
+    public List<Account> getAllAccounts(){
+        return null;
+    }
+
+    public Account inserAccount(Account account){
+        return null;
+    }
+
+    public Account getAccountByUsername(String username){
+        return null;
+    }
+
+    public Account getAccountById(int accId){
+        return null;
+    }
 }
