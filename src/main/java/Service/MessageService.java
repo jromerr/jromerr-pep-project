5dd974b1a79c812c643b20f207f70d7813e5102b
Jose Romero <romer243@csusm.edu>
2025-02-26 02:23:15 +0000

    Implement message service

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,48 @@
 package Service;
 
+import java.util.List;
+
+import DAO.MessageDAO;
+import Model.Message;
+
 public class MessageService {
-    
+    private MessageDAO messageDAO;
+
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO){
+        this.messageDAO = messageDAO;
+    }
+
+    public Message addMessage(Message message){
+        if(message.getMessage_text() != "" && message.getMessage_text().length() < 255 && messageDAO.getAllMessagesByAccountId(message.getPosted_by()) != null){
+            return messageDAO.insertMessage(message);
+        }
+        return null;
+    }
+
+    public List<Message> getAllMessages(){
+        return messageDAO.getAllMessages();
+    }
+
+    public Message getMessageByMessageId(Message message){
+        return messageDAO.getMessageByMessageId(message.getMessage_id());
+    }
+
+    public Message deleteMessage(Message message){
+        return messageDAO.deleteMessageByMessageId(message.getMessage_id());
+    }
+
+    public Message replaceMessage(Message message){
+        if(message.getMessage_text() != "" && message.getMessage_text().length() < 255 && messageDAO.getMessageByMessageId(message.getMessage_id()) != null){
+            return messageDAO.updateMessageByMessageId(message.getMessage_id(), message.getMessage_text());
+        }
+        return null;
+    }
+
+    public List<Message> getAllMessagesFromAccount(Message message){
+        return messageDAO.getAllMessagesByAccountId(message.getPosted_by());
+    }
 }
