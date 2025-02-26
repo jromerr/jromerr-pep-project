beec4da49ad4ea6e2c2a36662d706dffcdc6c5e2
Jose Romero <romer243@csusm.edu>
2025-02-26 01:49:43 +0000

    Implement account service

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,31 @@
 package Service;
 
+import DAO.AccountDAO;
+import Model.Account;
+
 public class AccountService {
-    
+    private AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    public Account addAccount(Account account){
+        if(account.getUsername() != "" && account.getPassword().length() >= 4 && accountDAO.getAccountByUsername(account.getUsername()) == null){
+            return accountDAO.insertAccount(account);
+        }
+        return null;
+    }
+
+    public Account loginAccount(Account account){
+        Account newAccount = accountDAO.getAccountByUsername(account.getUsername());
+        if (account.getPassword() == newAccount.getPassword()) {
+            return newAccount;
+        }
+        return null;
+    }
 }
